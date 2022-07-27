ff17ed15f7aa8fd99a54dfd8d21d0ec2e8cb5dc1
Felipe Tavares <xfelipetavares@gmail.com>
2022-07-27 06:44:23 -0300

    multiplicador feito e funcionando

--- scripts/main.js
+++ scripts/main.js
@@ -2,51 +2,55 @@ let resultDadoInput
 let num1
 let verifica100 = false
 let verificaSobeDesce = false
-let multiplicador = document.getElementById(`multiplicador`)
 
-function xamaAleatorio(verifica100, num1, resultDadoInput, multiplicador){
+function xamaAleatorio(verifica100, num1, resultDadoInput, multi){
     let resultDado = document.getElementById(`${resultDadoInput}`)
-    let multiNumber = parseInt(multiplicador.value)
-    let deucerto = false
-    
+    let multiNumber = parseInt(document.getElementById(multi).value)
 
+    multiplicador(multiNumber, resultDado, verifica100, num1)
+}
+
+function multiplicador(value, result, verifica100, num1){
+    var random = 0
+    for(let i = 0; i < value; i++){
+        random += randomNumber(verifica100, num1)
+    }
+    result.value = random
+}
+
+function randomNumber(verifica100, num1){
+    let deucerto = false
+    let resultReturn = 0
     if(verifica100 == true){
         do {
             let result = Math.floor(Math.random() * num1 + 1)
             if(result % 10 == 0){
-                resultDado.value = result
+               resultReturn = result
                 deucerto = true
             } 
     
         } while(deucerto == false)
     } else {
         let result = Math.floor(Math.random() * num1 + 1)
-        resultDado.value = result
+        resultReturn = result
     }
-    
-    
-
-
-    // for(let i = 0; i < multiNumber; i++){
-    //     console.log(result)
-    //     resultDadoSomadoP = resultDadoSomadoP + result
-    // }
-    
-    // resultDado.value = 
-    
+    console.log(resultReturn)
+    return resultReturn
 }
-
-
 
 function sobeDesce(multiplicador, verificaSobeDesce){
-    let multiNumber = parseInt(multiplicador.value)
-
+    let multi = document.getElementById(`${multiplicador}`)
+    let multiNumber = parseInt(multi.value)
     if(verificaSobeDesce == true){
         multiNumber++
-        multiplicador.value = multiNumber
+        multi.value = multiNumber
     } else {
         multiNumber--
-        multiplicador.value = multiNumber
+        multi.value = multiNumber
     }
 
 }
+
+// function cleaner(){
+
+// }
