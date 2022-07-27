549b9cdafefa8c54d68fb5dcd37ae22c93c8efe3
Felipe Tavares <xfelipetavares@gmail.com>
2022-07-27 08:36:30 -0300

    arrumado variaveis de cor no css

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,39 @@
-function xamaAleatorio(verifica100, num1, resultDadoInput, multi, resultDadoSomado){
+function xamaAleatorio(verifica100, num1, resultDadoInput, multi, resultDadoSomado) {
     let resultDado = document.getElementById(`${resultDadoInput}`)
     let multiNumber = parseInt(document.getElementById(multi).value)
     let resultDadoSomado2 = parseFloat(document.getElementById(`${resultDadoSomado}`).value)
 
-
     multiplicador(multiNumber, resultDado, verifica100, num1, resultDadoSomado2)
 }
 
-function multiplicador(value, result, verifica100, num1, resultDadoSomado2){
+
+function multiplicador(value, result, verifica100, num1, resultDadoSomado2) {
     var random = 0
-    for(let i = 0; i < value; i++){
+    for (let i = 0; i < value; i++) {
         random += randomNumber(verifica100, num1)
     }
 
-    if(resultDadoSomado2 >= 1){
+    if (resultDadoSomado2 >= 1) {
         result.value = random + resultDadoSomado2
     } else {
         result.value = random
     }
-    
+
 }
 
-function randomNumber(verifica100, num1){
+
+function randomNumber(verifica100, num1) {
     let deucerto = false
     let resultReturn = 0
-    if(verifica100 == true){
+    if (verifica100 == true) {
         do {
             let result = Math.floor(Math.random() * num1 + 1)
-            if(result % 10 == 0){
-               resultReturn = result
+            if (result % 10 == 0) {
+                resultReturn = result
                 deucerto = true
-            } 
-    
-        } while(deucerto == false)
+            }
+
+        } while (deucerto == false)
     } else {
         let result = Math.floor(Math.random() * num1 + 1)
         resultReturn = result
@@ -41,10 +42,11 @@ function randomNumber(verifica100, num1){
     return resultReturn
 }
 
-function sobeDesce(multiplicador, verificaSobeDesce){
+
+function sobeDesce(multiplicador, verificaSobeDesce) {
     let multi = document.getElementById(`${multiplicador}`)
     let multiNumber = parseInt(multi.value)
-    if(verificaSobeDesce == true){
+    if (verificaSobeDesce == true) {
         multiNumber++
         multi.value = multiNumber
     } else {
@@ -54,7 +56,8 @@ function sobeDesce(multiplicador, verificaSobeDesce){
 
 }
 
-function cleaner(resultado, multiplicador2, resultDadoSomado){
+
+function cleaner(resultado, multiplicador2, resultDadoSomado) {
     document.getElementById(`${resultado}`).value = ""
     document.getElementById(`${multiplicador2}`).value = "1"
     document.getElementById(`${resultDadoSomado}`).value = ""
